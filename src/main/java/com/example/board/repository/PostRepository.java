6a68508f25f69fa58b8c6b7b92e9f0278d53c705
sakurai <mene10morim@gmail.com>
2026-01-26 17:25:01 +0900

    掲示板の機能改善

--- src/main/java/com/example/board/repository/PostRepository.java
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.example.board.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,4 +19,19 @@ public interface PostRepository extends JpaRepository<Post, String> {
 	* @return 投稿
 	*/
 	public Optional<Post> findById(String id);
+
+	/**
+	* 更新日時の降順ですべての投稿を検索する
+	*
+	* @return 投稿のリスト
+	*/
+	List<Post> findAllByOrderByUpdatedDateDesc();
+
+	/**
+	* 更新日時の降順で未削除の投稿を検索する
+	*
+	* @return 投稿のリスト
+	*/
+	List<Post> findByDeletedFalseOrderByUpdatedDateDesc();
+
 }
