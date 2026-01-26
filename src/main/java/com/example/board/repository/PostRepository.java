6a8fe8c8150a3e1152ad34fadbf8b3f561cbcecf
sakurai <mene10morim@gmail.com>
2026-01-26 14:42:14 +0900

    掲示板の機能追加

--- src/main/java/com/example/board/repository/PostRepository.java
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -1,13 +1,21 @@
 package com.example.board.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.board.entity.Post;
+
 /**
  * 投稿のリポジトリー.
  */
 
 public interface PostRepository extends JpaRepository<Post, String> {
+	/**
+	* IDで検索する
+	*
+	* @param id ID
+	* @return 投稿
+	*/
+	public Optional<Post> findById(String id);
 }
-
-
